e9fe8c55ba11bb9d728284053c3b7dcd1173b2d5
Kohei Yamaguchi <kohei.yamaguchi.000@gmail.com>
2016-09-25 01:22:59 +0900

    Exclude user name and email in gitconfig

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+
 ln -s dotfiles/.vimrc ~/.vimrc
 ln -s dotfiles/.gitignore ~/.gitignore
+ln -s dotfiles/.gitconfig ~/.gitconfig
+ln -s dotfiles/.gitconfig.linux ~/.gitconfig.os
+cp    .gitconfig.user.template ~/.gitconfig.user
+echo "Finish deployment"
+echo "Edit your name and email in ~/.gitconfig.user"
