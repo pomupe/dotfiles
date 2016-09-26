30ae55354406a94f808fbb2371bdf4e19e886c6b
pomupe <2258971+pomupe@users.noreply.github.com>
2016-09-26 21:34:40 +0900

    Prompt before overwrite when .gitconfig.user is exist

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ ln -s dotfiles/dein.toml ~/dein.toml
 ln -s dotfiles/.gitignore ~/.gitignore
 ln -s dotfiles/.gitconfig ~/.gitconfig
 ln -s dotfiles/.gitconfig.linux ~/.gitconfig.os
-cp    .gitconfig.user.template ~/.gitconfig.user
+cp -i .gitconfig.user.template ~/.gitconfig.user
 echo "Finish deployment"
 echo "Edit your name and email in ~/.gitconfig.user"
