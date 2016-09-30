1f8244ee18350232346818a9ae4b5f8cd62d2ed4
pomupe <2258971+pomupe@users.noreply.github.com>
2016-10-01 00:35:30 +0900

    Update install.bat & replace tabs to spaces

--- bin/install.bat
+++ bin/install.bat
@@ -1,2 +1,12 @@
+@echo off
+: Vim
 mklink /H %HOME%\.vim\rc\dein.toml %HOME%\.dotfiles\config\vim\rc\dein.toml
-mklink /H %HOME%\_vimrc %HOME%\.dotfiles\config\vim\vimrc
+mklink /H %HOME%\_vimrc            %HOME%\.dotfiles\config\vim\vimrc
+
+: Git
+mklink /H %HOME%\.gitignore %HOME%\.dotfiles\config\git\gitignore
+mklink /H %HOME%\.gitconfig %HOME%\.dotfiles\config\git\gitconfig
+mklink /H %HOME%\.gitconfig.os %HOME%\.dotfiles\config\git\gitconfig.win
+if not exist %HOME%\.gitconfig.user (
+  copy %HOME%\.dotfiles\config\git\gitconfig.user.template %HOME%\.gitconfig.user
+)
