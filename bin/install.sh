fd02dc92252f1fa1ae66041c9cc1f5c1ac2b3aee
pomupe <2258971+pomupe@users.noreply.github.com>
2016-09-30 20:58:52 +0900

    Organize the directory structure

---
+++ bin/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+CONFIG_DIR=~/.dotfiles/config
+VIM_DIR=${CONFIG_DIR}/vim
+GIT_DIR=${CONFIG_DIR}/git
+TMUX_DIR=${CONFIG_DIR}/tmux
+
+# Vim
+ln -s ${VIM_DIR}/vimrc        ~/.vimrc
+if [ ! -e ~/.vim/rc ]; then
+  mkdir -p ~/.vim/rc
+fi
+ln -s ${VIM_DIR}/rc/dein.toml ~/.vim/rc/dein.toml
+
+# Git
+ln -s ${GIT_DIR}/gitignore               ~/.gitignore
+ln -s ${GIT_DIR}/gitconfig               ~/.gitconfig
+ln -s ${GIT_DIR}/gitconfig.linux         ~/.gitconfig.os
+if [ ! -f ~/.gitconfig.user ]; then
+  cp    ${GIT_DIR}/gitconfig.user.template ~/.gitconfig.user
+  echo "Create ~/.gitconfig.user"
+  echo "Edit your name and email in ~/.gitconfig.user"
+fi
+
+# tmux
+ln -s ${TMUX_DIR}/tmux.conf ~/.tmux.conf
+
+echo "Finish deployment"
