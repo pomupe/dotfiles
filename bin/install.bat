ebf397d59a1da147883cc3c844093963f55a1342
Kohei Yamaguchi <kohei.yamaguchi.000@gmail.com>
2016-10-01 17:57:29 +0900

    Update settings for Windows

--- bin/install.bat
+++ bin/install.bat
@@ -1,12 +1,19 @@
 @echo off
 : Vim
-mklink /H %HOME%\.vim\rc\dein.toml %HOME%\.dotfiles\config\vim\rc\dein.toml
-mklink /H %HOME%\_vimrc            %HOME%\.dotfiles\config\vim\vimrc
+mklink /H %HOMEPATH%\_vimrc %HOMEPATH%\.dotfiles\config\vim\vimrc
+if not exist %HOMEPATH%\.vim\rc\ (
+  mkdir %HOMEPATH%\.vim\rc
+)
+mklink /H %HOMEPATH%\.vim\rc\dein.toml %HOMEPATH%\.dotfiles\config\vim\rc\dein.toml
 
 : Git
-mklink /H %HOME%\.gitignore %HOME%\.dotfiles\config\git\gitignore
-mklink /H %HOME%\.gitconfig %HOME%\.dotfiles\config\git\gitconfig
-mklink /H %HOME%\.gitconfig.os %HOME%\.dotfiles\config\git\gitconfig.win
-if not exist %HOME%\.gitconfig.user (
-  copy %HOME%\.dotfiles\config\git\gitconfig.user.template %HOME%\.gitconfig.user
+mklink /H %HOMEPATH%\.gitignore    %HOMEPATH%\.dotfiles\config\git\gitignore
+mklink /H %HOMEPATH%\.gitconfig    %HOMEPATH%\.dotfiles\config\git\gitconfig
+mklink /H %HOMEPATH%\.gitconfig.os %HOMEPATH%\.dotfiles\config\git\gitconfig.win
+if not exist %HOMEPATH%\.gitconfig.user (
+  copy %HOMEPATH%\.dotfiles\config\git\gitconfig.user.template %HOMEPATH%\.gitconfig.user
+  echo "Create ~/.gitconfig.user"
+  echo "Edit your name and email in ~/.gitconfig.user"
 )
+
+echo "Finish deployment"
