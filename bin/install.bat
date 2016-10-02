a9dbdf1cdd91d0175cdb12a4d236bc83fb4ce068
Kohei Yamaguchi <kohei.yamaguchi.000@gmail.com>
2016-10-02 19:14:06 +0900

    Change hard link to symbolic link in install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -1,16 +1,16 @@
 @echo off
 : Vim
-mklink /H %HOMEPATH%\_vimrc %HOMEPATH%\.dotfiles\config\vim\vimrc
-mklink /H %HOMEPATH%\_gvimrc %HOMEPATH%\.dotfiles\config\vim\gvimrc
+mklink %HOMEPATH%\_vimrc %HOMEPATH%\.dotfiles\config\vim\vimrc
+mklink %HOMEPATH%\_gvimrc %HOMEPATH%\.dotfiles\config\vim\gvimrc
 if not exist %HOMEPATH%\.vim\rc\ (
   mkdir %HOMEPATH%\.vim\rc
 )
-mklink /H %HOMEPATH%\.vim\rc\dein.toml %HOMEPATH%\.dotfiles\config\vim\rc\dein.toml
+mklink %HOMEPATH%\.vim\rc\dein.toml %HOMEPATH%\.dotfiles\config\vim\rc\dein.toml
 
 : Git
-mklink /H %HOMEPATH%\.gitignore    %HOMEPATH%\.dotfiles\config\git\gitignore
-mklink /H %HOMEPATH%\.gitconfig    %HOMEPATH%\.dotfiles\config\git\gitconfig
-mklink /H %HOMEPATH%\.gitconfig.os %HOMEPATH%\.dotfiles\config\git\gitconfig.win
+mklink %HOMEPATH%\.gitignore    %HOMEPATH%\.dotfiles\config\git\gitignore
+mklink %HOMEPATH%\.gitconfig    %HOMEPATH%\.dotfiles\config\git\gitconfig
+mklink %HOMEPATH%\.gitconfig.os %HOMEPATH%\.dotfiles\config\git\gitconfig.win
 if not exist %HOMEPATH%\.gitconfig.user (
   copy %HOMEPATH%\.dotfiles\config\git\gitconfig.user.template %HOMEPATH%\.gitconfig.user
   echo "Create ~/.gitconfig.user"
