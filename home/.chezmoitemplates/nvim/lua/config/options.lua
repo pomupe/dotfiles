667398e5646aa3789d8e4fac77e77e31b44f6f1b
pomupe <2258971+pomupe@users.noreply.github.com>
2026-01-22 23:49:05 +0900

    feat: add neovim configurations

---
+++ home/.chezmoitemplates/nvim/lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+
+opt.conceallevel = 0
+opt.fileformats = "unix,dos"
+opt.matchtime = 1
+opt.scrolloff = 8
+opt.shiftwidth = 4
+opt.showmatch = true
+opt.smoothscroll = false
+opt.tabstop = 4
+opt.wildignorecase = true
+opt.wrap = true
---
+++ home/.chezmoitemplates/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+    "mason-org/mason.nvim",
+    opts = {
+        ensure_installed = {
+            "lua-language-server",
+            "markdown-oxide",
+            "ruff",
+            "rust-analyzer",
+            "tombi",
+            "ty",
+        },
+    },
+}
